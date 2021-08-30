cafe7ffa9510e3ae86ed41293c40ce0635992dbf
yuko-kochi <happy.k165131@gmail.com>
2021-08-30 11:23:42 +0000

    Finish toy app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    render html: "hola, mundo!"
+    render html: "hello, world!"
   end
 
   def goodbye
